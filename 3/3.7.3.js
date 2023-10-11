e8b73e798e63e07ec084442086b791ff686a3f95
Ksenia Peresedova <kshusya.yay@gmail.com>
2023-10-11 15:54:08 +0400

    fixed 3.7.3

--- 3/3.7.3.js
+++ 3/3.7.3.js
@@ -5,7 +5,7 @@ let num2 = 400
 let arr = []
 let smallestNum = Math.min(num1, num2)
 
-for (let i = 0; i < smallestNum; i++) {
+for (let i = 1; i <= smallestNum; i++) {
     if (num1 % i === 0 && num2 % i === 0) arr.push(i)
 }
 
